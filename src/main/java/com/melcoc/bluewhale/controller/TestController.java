d15bf16c673ba2b16b2f641c1500d8da27b2d38f
he_you_long <2899920172@qq.com>
2020-04-27 22:17:10 +0800

    提交Follow

--- src/main/java/com/melcoc/bluewhale/controller/TestController.java
+++ src/main/java/com/melcoc/bluewhale/controller/TestController.java
@@ -2,6 +2,7 @@ package com.melcoc.bluewhale.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
@@ -13,4 +14,12 @@ public class TestController {
     public String reg(){
         return "reg";
     }
+
+    @RequestMapping("/test")
+    public ModelAndView logina(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("test");
+        return modelAndView;
+    }
+
 }
