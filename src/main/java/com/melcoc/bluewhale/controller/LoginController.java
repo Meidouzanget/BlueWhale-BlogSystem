d40ff88c9ffddff302f1f9eecd13c3a7c08f1543
Melco <983978565@qq.com>
2020-06-01 22:44:04 +0800

    初步完成Shiro搭建

--- src/main/java/com/melcoc/bluewhale/controller/LoginController.java
+++ src/main/java/com/melcoc/bluewhale/controller/LoginController.java
@@ -1,17 +1,71 @@
 package com.melcoc.bluewhale.controller;
 
-import com.melcoc.bluewhale.domain.Article;
-import com.melcoc.bluewhale.domain.Img;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.melcoc.bluewhale.dao.ResponseBean;
+import com.melcoc.bluewhale.domain.LUser;
+import com.melcoc.bluewhale.jwt.JWTUtil;
+import com.melcoc.bluewhale.serviceImpl.UserService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.UnauthorizedException;
+import org.apache.shiro.authz.annotation.Logical;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class LoginController {
 
-    @RequestMapping("/login")
-    public String login(String userName,String userPassord){
+    @Autowired
+    private UserService service;
 
-        return null;
+    @PostMapping("/login")
+    public ResponseBean login(@RequestParam("username") String username,
+                              @RequestParam("password") String password) {
+        LUser userBean = service.getUserWithPermission(username);
+        if (userBean.getPassword().equals(password)) {
+            return new ResponseBean(200, "Login successfully", JWTUtil.sign(username, password));
+        } else {
+            throw new UnauthorizedException();
+        }
+    }
+
+    @GetMapping("/article")
+    public ResponseBean article() {
+        Subject subject = SecurityUtils.getSubject();
+        if (subject.isAuthenticated()) {
+            return new ResponseBean(200, "You are already logged in", null);
+        } else {
+            return new ResponseBean(200, "You are guest", null);
+        }
+    }
+
+
+    @GetMapping("/require_auth")
+    @RequiresAuthentication
+    public ResponseBean requireAuth() {
+        return new ResponseBean(200, "You are authenticated", null);
+    }
+
+    @GetMapping("/require_role")
+    @RequiresRoles(value = {"admin","ADMIN"},logical = Logical.OR)
+    public ResponseBean requireRole() {
+        Subject subject = SecurityUtils.getSubject();
+        return new ResponseBean(200, "You are visiting require_role", null);
+    }
+
+    @GetMapping("/require_permission")
+    @RequiresPermissions(logical = Logical.AND, value = {"view", "edit"})
+    public ResponseBean requirePermission() {
+        return new ResponseBean(200, "You are visiting permission require edit,view", null);
+    }
+
+    @RequestMapping(path = "/401")
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseBean unauthorized() {
+        return new ResponseBean(401, "Unauthorized", null);
     }
 
 
---
+++ src/main/java/com/melcoc/bluewhale/domain/LRolepermission.java
@@ -0,0 +1,37 @@
+package com.melcoc.bluewhale.domain;
+
+
+public class LRolepermission {
+
+  private long rpid;
+  private long rid;
+  private long pid;
+
+
+  public long getRpid() {
+    return rpid;
+  }
+
+  public void setRpid(long rpid) {
+    this.rpid = rpid;
+  }
+
+
+  public long getRid() {
+    return rid;
+  }
+
+  public void setRid(long rid) {
+    this.rid = rid;
+  }
+
+
+  public long getPid() {
+    return pid;
+  }
+
+  public void setPid(long pid) {
+    this.pid = pid;
+  }
+
+}
