271519b76849209a1edbe7d09a7383140ed504de
Melco <983978565@qq.com>
2020-06-18 15:29:44 +0800

    清空test

--- src/test/java/com/melcoc/bluewhale/BluewhaleApplicationTests.java
+++ src/test/java/com/melcoc/bluewhale/BluewhaleApplicationTests.java
@@ -1,44 +1,9 @@
 package com.melcoc.bluewhale;
 
-import com.melcoc.bluewhale.domain.LUserrole;
-import com.melcoc.bluewhale.service.ArticleService;
-import com.melcoc.bluewhale.service.UserService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
 class BluewhaleApplicationTests {
-    @Autowired
-    ArticleService articleService;
-    @Autowired
-    UserService userService;
-
-
-
-    @Test
-    void testQiniu() {
-
-
-
-
-    }
-
-    @Test
-    void login(){
-        //LUser lUser = userDao.getUserWithPermission("clarence");
-        System.out.println();
-    }
-    @Test
-    void register(){
-        LUserrole lUserrole = new LUserrole();
-
-        lUserrole.setRid(1);
-        lUserrole.setUrid(1);
-
-        userService.insertUserrole(lUserrole);
-        System.out.println(lUserrole);
-
-    }
+    
 }
